bf3d086e69c0414108e91ae1dc3104fccfb5695d
mouratsa <thiago.smoura@outlook.com>
2025-01-23 09:46:31 -0300

    criado alert campo amigo

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = []
 
+function adicionarAmigo(){
+    const nomeDoAmigo = document.getElementById("amigo");
+    const nome = nomeDoAmigo.value.trim();
+
+    if (nome == ""){
+        alert("Favor informar um nome");
+        return;
+    }
+
+    amigos.push(nome);
+    console.log(amigos);
+
+    nomeDoAmigo.value = "";
+    exibirAmigos();
+}
